575cfd72bfc67890d39fcea7fb8ef926de62cc0d
rupeshirl <rupesharunachalam@gmail.com>
2021-10-07 16:15:25 +0530

    my first git

---
+++ src/org/CollectionList/Numbersclass.java
@@ -0,0 +1,30 @@
+package org.CollectionList;
+
+
+import java.util.*;
+
+public class Numbersclass {
+	
+	public static void main(String[] args) {
+		
+		
+		List<Integer> li=new ArrayList<Integer>();
+		
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(40);
+		li.add(50);
+		
+		System.out.println(li);
+		
+		
+		int siz= li.size();
+		System.out.println(siz);
+		
+		
+		
+		
+	}
+
+}
